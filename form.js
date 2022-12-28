c1946f72d86a9cae2d0f9bbddf1699321cf0bce2
jairoalperez <jairoalberto98@hotmail.com>
2022-12-28 16:28:20 -0500

    Form with bootstrap

---
+++ form.js
@@ -0,0 +1,19 @@
+function submitInfo() {
+    var name = document.getElementById('name').value
+    var number = document.getElementById('number').value
+    var email = document.getElementById('email').value
+    var dob = document.getElementById('dob').value
+    var doj = document.getElementById('doj').value
+    var exp = document.getElementById('exp').value
+
+    alert('Full Name: '+name+'\n\nPhone Number: '+number+'\n\nEmail: '+email+'\n\nDate of Birth: '+dob+'\n\nDate of Joining: '+doj+'\n\nExpertise: '+exp)
+
+}
+
+
+
+
+
+
+
+
