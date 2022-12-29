ee292343c3d59c6227aa9496ba4b9a044fbf985b
jairoalperez <jairoalberto98@hotmail.com>
2022-12-29 01:11:30 -0500

    adding table to show users

--- form/form.js
+++ form/form.js
@@ -1,4 +1,7 @@
 function submitInfo() {
+
+    var Num = 1
+
     var name = document.getElementById('name').value
     var number = document.getElementById('number').value
     var email = document.getElementById('email').value
@@ -6,7 +9,22 @@ function submitInfo() {
     var doj = document.getElementById('doj').value
     var exp = document.getElementById('exp').value
 
-    alert('Full Name: '+name+'\n\nPhone Number: '+number+'\n\nEmail: '+email+'\n\nDate of Birth: '+dob+'\n\nDate of Joining: '+doj+'\n\nExpertise: '+exp)
+    var tbody = document.getElementById('tbody')
+
+    alert('Full Name: ' + name + '\n\nPhone Number: ' + number + '\n\nEmail: ' + email + '\n\nDate of Birth: ' + dob + '\n\nDate of Joining: ' + doj + '\n\nExpertise: ' + exp)
+
+    tbody.innerHTML += `
+         <tr>
+            <th scope="row">${Num}</th>
+            <td>${name}</td>
+            <td>${number}</td>
+            <td>${email}</td>
+            <td>${dob}</td>
+            <td>${doj}</td>
+            <td>${exp}</td>
+         </tr>
+        `
+
 
 }
 
